6fb924785bad3f11c029e99bed40647841b6e405
Minh Nguyen <nguyen.715@osu.edu>
2020-07-14 22:31:43 -0400

    boilerplate

--- src/app.js
+++ src/app.js
@@ -1,19 +1,69 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const winston = require('winston');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 
 const app = express();
 
+const bookmarks = [
+  {
+    id: 0,
+    title: 'Google',
+    url: 'http://www.google.com',
+    rating: '3',
+    desc: 'Internet-related services and products.'
+  },
+  {
+    id: 1,
+    title: 'Thinkful',
+    url: 'http://www.thinkful.com',
+    rating: '5',
+    desc: '1-on-1 learning to accelerate your way to a new high-growth tech career!'
+  },
+  {
+    id: 2,
+    title: 'Github',
+    url: 'http://www.github.com',
+    rating: '4',
+    desc: 'brings together the world\'s largest community of developers.'
+  }
+];
+
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
+});
+
+if (NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+});
 
 app.get('/', (req,res) => {
   res.send('Hello, world!')
