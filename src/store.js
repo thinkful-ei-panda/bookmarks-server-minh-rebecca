bc2f71fc30c7e6919a2d8a37002c25560aba907a
Rhudson <contact@rebeccahudson.me>
2020-08-03 06:08:06 -0400

    fixed folder naming & uuid error

--- src/store.js
+++ src/store.js
@@ -1,27 +1,21 @@
-const uuid = require('v4: uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const bookmarks = [
-    {
-      id: uuid(),
-      title: 'Google',
-      url: 'http://www.google.com',
-      rating: '3',
-      desc: 'Internet-related services and products.'
-    },
-    {
-      id: uuid(),
-      title: 'Thinkful',
-      url: 'http://www.thinkful.com',
-      rating: '5',
-      desc: '1-on-1 learning to accelerate your way to a new high-growth tech career!'
-    },
-    {
-      id: uuid(),
-      title: 'Github',
-      url: 'http://www.github.com',
-      rating: '4',
-      desc: 'brings together the world\'s largest community of developers.'
-    }
-];
+  { id: uuidv4(),
+    title: 'Thinkful',
+    url: 'https://www.thinkful.com',
+    description: 'Think outside the classroom',
+    rating: 5 },
+  { id: uuidv4(),
+    title: 'Google',
+    url: 'https://www.google.com',
+    description: 'Where we find everything else',
+    rating: 4 },
+  { id: uuidv4(),
+    title: 'MDN',
+    url: 'https://developer.mozilla.org',
+    description: 'The only place to find web documentation',
+    rating: 5 },
+]
 
 module.exports = { bookmarks }
